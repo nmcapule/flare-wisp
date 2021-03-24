311cfab61005807e7c1623d1f53e1973ad420f38
Nathaniel M. Capule <nmcapule@gmail.com>
2021-03-24 18:40:39 +0000

    try css modules

---
+++ components/Alert.tsx
@@ -0,0 +1,5 @@
+import styles from "./Alert.module.scss";
+
+export default function Alert() {
+  return <p className={styles.text}>Alert!</p>;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import Alert from "../components/Alert";
+
 export default function Index() {
-  return <h1>You're running Reat on the Edge!</h1>;
+  return (
+    <>
+      <h1>You're running Reat on the Edge!</h1>
+      <Alert></Alert>
+    </>
+  );
 }
